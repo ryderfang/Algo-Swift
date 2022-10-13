f8091eabe324d14b1b7b45c8a63b95c0d4d6c0b1
ryderfang <ryfang@coupang.com>
2022-10-13 19:19:41 +0800

    [LeetCode] refine code of e13

--- Swift-Solutions/Swift-Solutions/LeetCode/Easy/13.roman-to-integer.swift
+++ Swift-Solutions/Swift-Solutions/LeetCode/Easy/13.roman-to-integer.swift
@@ -12,60 +12,40 @@ fileprivate extension String {
     }
 }
 
+fileprivate extension Character {
+    func digit() -> Int {
+        switch self {
+        case "M":
+            return 1000
+        case "D":
+            return 500
+        case "C":
+            return 100
+        case "L":
+            return 50
+        case "X":
+            return 10
+        case "V":
+            return 5
+        case "I":
+            return 1
+        default:
+            return -1
+        }
+    }
+}
+
 class Solution {
     func romanToInt(_ s: String) -> Int {
         var res = 0
-        var i = 0
-        let sz = s.count
-        while i < sz{
-            let ch: Character = s[i]
-            var nextCh: Character? = nil
-            if i < sz - 1 {
-                nextCh = s[i+1]
+        let n = s.count
+        for i in 0..<n {
+            let d = s[i].digit()
+            if i < n - 1 && s[i+1].digit() > d {
+                res -= d
+            } else {
+                res += d
             }
-            switch ch {
-            case "M":
-                res += 1000
-            case "D":
-                res += 500
-            case "C":
-                if nextCh == "D" {
-                    res += 400
-                    i += 1
-                } else if nextCh == "M" {
-                    res += 900
-                    i += 1
-                } else {
-                    res += 100
-                }
-            case "L":
-                res += 50
-            case "X":
-                if nextCh == "L" {
-                    res += 40
-                    i += 1
-                } else if nextCh == "C" {
-                    res += 90
-                    i += 1
-                } else {
-                    res += 10
-                }
-            case "V":
-                res += 5
-            case "I":
-                if nextCh == "V" {
-                    res += 4
-                    i += 1
-                } else if nextCh == "X" {
-                    res += 9
-                    i += 1
-                } else {
-                    res += 1
-                }
-            default:
-                break
-            }
-            i += 1
         }
         return res
     }
