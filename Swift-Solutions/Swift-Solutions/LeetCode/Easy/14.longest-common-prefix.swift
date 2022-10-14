ebca767d8eeb9f657a8cad1a654899756ca8b152
ryderfang <ryfang@coupang.com>
2022-10-14 11:47:26 +0800

    [LeetCode] e14: elegant solution

--- Swift-Solutions/Swift-Solutions/LeetCode/Easy/14.longest-common-prefix.swift
+++ Swift-Solutions/Swift-Solutions/LeetCode/Easy/14.longest-common-prefix.swift
@@ -5,39 +5,13 @@
  */
 
 // @lc code=start
-fileprivate extension String {
-    // 扩展下标方法
-    subscript (i: Int) -> Character {
-        return self[self.index(self.startIndex, offsetBy: i)]
-    }
-}
-
 class Solution {
     func longestCommonPrefix(_ strs: [String]) -> String {
-        var ret = ""
-        var i = 0
-        while true {
-            var ch: Character? = nil
-            var match = true
-            for s in strs {
-                if i < s.count {
-                    if ch == nil {
-                        ch = s[i]
-                    } else if ch != s[i] {
-                        match = false
-                        break
-                    }
-                } else {
-                    match = false
-                    break
-                }
+        guard var ret = strs.first else { return "" }
+        for s in strs.dropFirst() {
+            while !s.hasPrefix(ret) {
+                ret.removeLast()
             }
-            if match {
-                ret.append(ch!)
-            } else {
-                break
-            }
-            i += 1
         }
         return ret
     }
