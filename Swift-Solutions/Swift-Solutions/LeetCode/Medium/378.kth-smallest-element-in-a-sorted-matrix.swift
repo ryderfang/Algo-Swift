ad79333d184776eed9f87e13d60c55a3606fed28
Rui Fang <fray9166@gmail.com>
2023-12-07 17:56:48 +0800

    [LeetCode] m378 - update

--- Swift-Solutions/Swift-Solutions/LeetCode/Medium/378.kth-smallest-element-in-a-sorted-matrix.swift
+++ Swift-Solutions/Swift-Solutions/LeetCode/Medium/378.kth-smallest-element-in-a-sorted-matrix.swift
@@ -43,10 +43,131 @@ extension Solution {
 }
 
 // MARK: Solution1 - Divide and Conquer
+// http://www.cse.yorku.ca/~andy/pubs/X+Y.pdf
+extension Solution {
+    func kthSmallest1(_ matrix: [[Int]], _ k: Int) -> Int {
+        let n = matrix.count
+        guard k >= 1 && k <= n * n else { return -1 }
+        let start = max(k - n * n + n  - 1, 0)
+        let k = k - n * n + (n - start) * (n - start)
+//        let start = 0
+        let (result, _) = _biselect1(Array(start..<min(n, start + k)), matrix, k, k)
+        return result
+    }
+
+    func _biselect1(_ index: [Int], _ mat: [[Int]], _ k1: Int, _ k2: Int) -> (Int, Int) {
+        let n = index.count
+        let isEven = (n % 2 == 0)
+        let A: (Int, Int) -> Int = { (_ i: Int, _ j: Int) in
+            mat[index[i]][index[j]]
+        }
+        if n <= 2 {
+            var nums = [Int]()
+            for i in 0..<n {
+                for j in 0..<n {
+                    nums.append(A(i, j))
+                }
+            }
+            nums.sort()
+            return (nums[k1-1], nums[k2-1])
+        }
+
+        // sub problem
+        // [0,1,2...] -> [0,2,...(n-1)]
+        var index_ = [Int]()
+        for i in stride(from: 0, to: n, by: 2) {
+            index_.append(index[i])
+        }
+        if isEven {
+            index_.append(index[n-1])
+        }
+        let k1_ = isEven ? (n + 1 + (k1 + 3) / 4) : ((k1 + 2 * n) / 4 + 1)
+        let k2_ = (k2 + 3) / 4
+        let (a, b) = _biselect1(index_, mat, k1_, k2_)
+
+        // calc
+        var (ra_less, rb_more) = (0, 0)
+        var L = [Int]()
+        // ja - first element larger or equal to a
+        // jb - first element larger than b
+        var (jb, ja) = (n, n)
+        for i in 0..<n {
+            while jb > 0 && A(i, jb-1) > b {
+                jb -= 1
+            }
+            while ja > 0 && A(i, ja-1) >= a {
+                ja -= 1
+            }
+            ra_less += ja
+            rb_more += (n - jb)
+            if ja > jb {
+                L += (jb..<ja).map { A(i, $0) }
+            }
+        }
+        var (x, y) = (0, 0)
+        if ra_less <= k1 - 1 {
+            x = a
+        } else if k1 + rb_more - n * n <= 0 {
+            x = b
+        } else {
+            x = _pick(L, k1 + rb_more - n * n)
+        }
+        if ra_less <= k2 - 1 {
+            y = a
+        } else if k2 + rb_more - n * n <= 0 {
+            y = b
+        } else {
+            y = _pick(L, k2 + rb_more - n * n)
+        }
+        return (x, y)
+    }
+
+    // O(len(L)) - kth element of list
+    // median of medians algorithm
+    func _pick(_ list: [Int], _ k: Int) -> Int {
+        let n = list.count
+//        return list.sorted()[k-1]
+        assert(n > 0)
+        if k == 1 {
+            return list.min()!
+        }
+        var groups = [[Int]]()
+        var medians = [Int]()
+        for i in stride(from: 0, to: n, by: 5) {
+            let g = Array(list[i..<min(i+5,n)]).sorted()
+            groups.append(g)
+            medians.append(g[g.count / 2])
+        }
+        let pivot = _pick(medians, medians.count / 2 + 1)
+        var less = [Int]()
+        var equal = [Int]()
+        var greater = [Int]()
+        for x in list {
+            if x > pivot {
+                greater.append(x)
+            } else if x < pivot {
+                less.append(x)
+            } else {
+                equal.append(x)
+            }
+        }
+        if k <= less.count {
+            return _pick(less, k)
+        }
+        let k_ = k - less.count - equal.count
+        if k_ < 1 {
+            return pivot
+        } else {
+            return _pick(greater, k_)
+        }
+    }
+}
+
+// MARK: Solution2 - Divide and Conquer
 // https://chaoxu.prof/posts/2014-04-02-selection-in-a-sorted-matrix.html
 extension Solution {
     // X + Y 问题 (http://www.cse.yorku.ca/~andy/pubs/X+Y.pdf)
-    func kthSmallest1(_ matrix: [[Int]], _ k: Int) -> Int {
+    func kthSmallest2(_ matrix: [[Int]], _ k: Int) -> Int {
         let n = matrix.count
         let m = matrix[0].count
         guard k >= 1 && k <= n * m else { return -1 }
@@ -157,10 +278,10 @@ extension Array where Element == [Int] {
     }
 }
 
-// MARK: Solution2 - Priority Queue
+// MARK: Solution3 - Priority Queue
 // O(klogn) / O(k)
 extension Solution {
-    func kthSmallest2(_ matrix: [[Int]], _ k: Int) -> Int {
+    func kthSmallest3(_ matrix: [[Int]], _ k: Int) -> Int {
         let n = matrix.count
         typealias Pair = (i: Int, j: Int, v: Int)
         var pq = PriorityQueue<Pair>([], { $0.v < $1.v })
@@ -254,4 +375,3 @@ fileprivate struct PriorityQueue<Element> {
 }
 
 // @lc code=end
-
